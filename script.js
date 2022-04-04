05b87cbfaf99de3c5ffa69752396cbc6776c6f18
Aimal Amiri <meaimal@hotmail.com>
2022-04-05 04:14:48 +0430

    Fix linter errors

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-let navMenu = document.querySelector('#nav-menu');
-let menuBtn = document.querySelector('#menu-btn');
-let menuIcon = document.querySelector('#menu-icon');
-let navLinks =document.querySelectorAll('.navbar-link a');
-
-menuBtn.addEventListener('click', function(e){
-  navMenu.classList.toggle('d-block')
-  swapMenuIcon();
-});
-
-for(let link of navLinks){
-  link.addEventListener('click', function(e){
-     navMenu.classList.remove('d-block');
-     swapMenuIcon();
-  });
-}
+const navMenu = document.querySelector('#nav-menu');
+const menuBtn = document.querySelector('#menu-btn');
+const menuIcon = document.querySelector('#menu-icon');
+const navLinks = document.querySelectorAll('.navbar-link a');
 
 function swapMenuIcon() {
-  if(navMenu.classList.contains('d-block')){
+  if (navMenu.classList.contains('d-block')) {
     menuIcon.src = './assets/png/close.png';
-  }else{
-   menuIcon.src ='./assets/svg/menu.svg';
+  } else {
+    menuIcon.src = './assets/svg/menu.svg';
   }
 }
 
+menuBtn.addEventListener('click', () => {
+  navMenu.classList.toggle('d-block');
+  swapMenuIcon();
+});
 
+for (let i = 0; i < navLinks.length; i += 1) {
+  navLinks[i].addEventListener('click', () => {
+    navMenu.classList.remove('d-block');
+    swapMenuIcon();
+  });
+}
